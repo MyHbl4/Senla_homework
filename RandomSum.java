fd05711d275eced7ca8a92e3cf149219bc8410d8
Aleksandr Petushenko <aleksandr.petushenko1@gmail.com>
2021-09-09 11:36:39 +0300

    change random

---
+++ RandomSum.java
@@ -0,0 +1,39 @@
+public class RandomSum {
+    private int sumNum;
+    private final int random;
+
+    public RandomSum() {
+        this.random = (new java.util.Random()).nextInt(900) + 100;
+        this.sumNum = sumRandomNum();
+    }
+
+    public int sumRandomNum() {
+        String str = "" + random;
+        int one = str.charAt(str.length() - 3) - '0';
+        int two = str.charAt(str.length() - 2) - '0';
+        int three = str.charAt(str.length() - 1) - '0';
+        sumNum = one+two+three;
+        return sumNum;
+    }
+
+    public int getSumNum() {
+        return sumNum;
+    }
+
+    public int getRandom() {
+        return random;
+    }
+
+
+    @Override
+    public String toString() {
+        return "RandomSum [ "+ "Random number: " + random + ", Sum numbers: " + sumNum +']';
+    }
+
+    public static void main(String[] args) {
+        RandomSum random = new RandomSum();
+        System.out.println(random);
+        System.out.println(random.getRandom());
+        System.out.println(random.getSumNum());
+    }
+}
