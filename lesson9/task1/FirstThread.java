6123cb1101e1a4d5d5709bd01db3962599672e7f
Aleksandr Petushenko <aleksandr.petushenko1@gmail.com>
2021-10-22 23:12:22 +0300

    9 remix

--- lesson9/task1/FirstThread.java
+++ lesson9/task1/FirstThread.java
@@ -2,9 +2,25 @@ package lesson9.task1;
 
 public class FirstThread extends Thread {
 
+  private static final Object object = new Object();
+
+  public static Object getObject() {
+    return object;
+  }
+
+  @Override
   public void run() {
+//    try {
+//      Thread.sleep(1000);
+//    } catch (Exception e) {
+//      e.printStackTrace();
+//    }
     try {
       Thread.sleep(1000);
+//      Thread.sleep(1000);
+      synchronized (object) {
+        object.wait();
+      }
     } catch (Exception e) {
       e.printStackTrace();
     }
--- lesson9/task1/StateThread.java
+++ lesson9/task1/StateThread.java
@@ -1,26 +1,36 @@
 package lesson9.task1;
 
+import static java.lang.Thread.sleep;
+
 public class StateThread {
   private final String name = "State thread";
 
   public static void main(String[] args) {
     FirstThread ft = new FirstThread();
-    SecondThread st = new SecondThread();
     StateThread state = new StateThread();
     try {
-      state.print(ft);
+      state.print(ft);/////////////////////////////////////new
+
       ft.start();
-      state.print(ft);
-      st.start();
-      Thread.sleep(500);
-      state.print(ft);
-      state.print(st);
-      Thread.sleep(3000);
-      synchronized (SecondThread.getObject()) {
-        SecondThread.getObject().notify();
+      state.print(ft);/////////////////////////////////////runnable
+
+      sleep(500);
+      state.print(ft);/////////////////////////////////////time-waiting
+
+      sleep(3000);
+      state.print(ft);/////////////////////////////////////waiting
+      synchronized (FirstThread.getObject()) {
+        FirstThread.sleep(1000);
+        FirstThread.getObject().notify();
+        state.print(ft);////////////////////////////////////blocked
       }
-      state.print(st);
-      state.print(ft);
+      synchronized (FirstThread.getObject()) {
+        FirstThread.sleep(1000);
+        FirstThread.getObject().notify();
+      }
+      sleep(2000);
+      state.print(ft);////////////////////////////////////terminated
+
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
