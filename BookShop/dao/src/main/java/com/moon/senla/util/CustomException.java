05ccf18bde0c7b74e732c410fd080956b21b99db
Aleksandr Petushenko <aleksandr.petushenko1@gmail.com>
2021-11-24 01:01:49 +0300

    Sprng MVC #4

--- BookShop/dao/src/main/java/com/moon/senla/util/CustomException.java
+++ BookShop/dao/src/main/java/com/moon/senla/util/CustomException.java
@@ -1,9 +1,8 @@
 package com.moon.senla.util;
 
-import java.io.Serial;
 
 public class CustomException extends Exception {
-    @Serial
+
     private static final long serialVersionUID = 1000000000L;
 
     public CustomException() {
